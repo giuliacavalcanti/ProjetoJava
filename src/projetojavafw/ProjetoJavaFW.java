486238a51a6d5555663c35048359107cc37ec68a
Rebecca Andrade <rebecca.a0619@gmail.com>
2018-07-17 12:38:31 -0300

    reorganizacao das packages(remocao)

--- src/projetojavafw/ProjetoJavaFW.java
+++ src/projetojavafw/ProjetoJavaFW.java
@@ -1,64 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetojavafw;
 
-import conta.Conta;
-import conta.Poupanca;
-import repositorio.array.RepositorioContasArray;
-
-/**
- *
- * @author 1545-6 IRON V4
- */
 public class ProjetoJavaFW {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-//        Conta c = new Conta(20.0,"0");
-//        RepositorioContasArray banco = new RepositorioContasArray();
-//        banco.inserir(c);
-//        c = new Conta(30.0,"1");
-//        banco.inserir(c);
-//        c= new Poupanca(35.0,"2");
-//        banco.inserir(c);
-//        c =  new Poupanca(22.0,"3");
-//        banco.inserir(c);
-//        c = new Conta(10.0,"4");
-//        banco.inserir(c);
-//        c = new Poupanca(40.0,"5");
-//        banco.inserir(c);
-//        
-//        Conta[] contas = banco.getContas();
-//        System.out.println("valores e contas:"+
-//                '\n'+"0:"+banco.procurar("0").getSaldo()+
-//                '\n'+"1:"+banco.procurar("1").getSaldo()+
-//                '\n'+"2:"+banco.procurar("2").getSaldo()+
-//                '\n'+"3:"+banco.procurar("3").getSaldo()+
-//                '\n'+"4:"+banco.procurar("4").getSaldo()+
-//                '\n'+"5:"+banco.procurar("5").getSaldo());
-//        realizarRenderJuros(banco);
-//        System.out.println("valores atualizados:"+
-//                '\n'+"0:"+banco.procurar("0").getSaldo()+
-//                '\n'+"1:"+banco.procurar("1").getSaldo()+
-//                '\n'+"2:"+banco.procurar("2").getSaldo()+
-//                '\n'+"3:"+banco.procurar("3").getSaldo()+
-//                '\n'+"4:"+banco.procurar("4").getSaldo()+
-//                '\n'+"5:"+banco.procurar("5").getSaldo());
-//    }
-//    
-//    public static void realizarRenderJuros(RepositorioContasArray banco){
-//        Poupanca p;
-//        Conta contas[] = banco.getContas();
-//        for(int i=0;i<6;i++){
-//            if(contas[i] instanceof Poupanca){
-//               p = (Poupanca) contas[i];
-//               p.renderJuros(0.01);
-//            }
-//        }
+        
     }
 }
