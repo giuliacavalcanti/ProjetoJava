4e70f8888a6422db17496d0b5da83e939842d82c
Rebecca Andrade <rebecca.a0619@gmail.com>
2018-07-18 20:05:23 -0300

    implementacao basica do hashmap de conta e treeset de cliente(precisa completar)

---
+++ src/dados/repositorio/hashmap/RepositorioContasHashmap.java
@@ -0,0 +1,76 @@
+package dados.repositorio.hashmap;
+
+import dados.repositorio.interfaces.IRepConta;
+import modelo.conta.Conta;
+import java.util.*;
+
+public class RepositorioContasHashmap implements IRepConta{
+
+    Set contas = new HashSet();
+    int indice;
+
+    public Set getContas() {
+        return contas;
+    }
+
+    public void setContas(Set contas) {
+        this.contas = contas;
+    }
+
+    public int getIndice() {
+        return indice;
+    }
+
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+
+    @Override
+    public void inserir(Conta conta) {
+        contas.add(conta);
+    }
+
+    @Override
+    public void remover(Conta conta) {
+        contas.remove(conta);
+    }
+
+    @Override
+    public void atualizar(Conta conta) {
+        remover(procurar(conta.getNumero()));
+        inserir(conta);
+    }
+
+    @Override
+    public Conta procurar(String num_conta) {
+        Iterator it = contas.iterator();
+        Conta c;
+        while(true) {
+            c = (Conta) it.next();
+            if(c.getNumero().equals(num_conta))
+                return c;
+        }
+    }
+
+    @Override
+    public int procurarIndice(String num_conta) {
+        Iterator it = contas.iterator();
+        Conta c;
+        for(int i = 0;;i++) {
+            c = (Conta) it.next();
+            if(c.getNumero().equals(num_conta))
+                return i;
+        }
+    }
+
+    @Override
+    public boolean existe(String num_conta) {
+        return contas.contains(procurar(num_conta));
+    }
+
+    @Override
+    public int getTamCacheContas() {
+        return TAM_CACHE_CONTAS;
+    }
+
+}
