b9bf3da53bb26724b542a456a75d3c65fce7d02c
Lucas Lima <Lucaspitt@hotmail.com>
2018-07-18 20:39:01 -0300

    Remoção das assinaturas dos metodos Get e Set das Interfaces

--- src/dados/repositorio/interfaces/IRepCliente.java
+++ src/dados/repositorio/interfaces/IRepCliente.java
@@ -5,17 +5,11 @@ import modelo.cliente.Cliente;
 public interface IRepCliente {
     
     public static final int TAM_CACHE_CLIENTES = 100;
-//    public Cliente[] getClientes();
-//    public void setClientes(Cliente[] clientes);
-    public int getIndice();
-    public void setIndice(int indice); 
-//    public boolean isFull();
     public void inserir(Cliente cliente);
     public void remover(Cliente cliente);
     public void atualizar(Cliente cliente);
     public Cliente procurar(String cpf);
     public int procurarIndice(String cpf);
     public boolean existe(String cpf);
-    public int getTamCacheClientes();
 	
 }
