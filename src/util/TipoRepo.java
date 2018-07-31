9fdb62a442e24c1996355c5d457e52ea00aa3670
Lucas Lima <Lucaspitt@hotmail.com>
2018-07-31 15:15:22 -0300

    Correções para enum TipoRepo em FactoryCliente e FactoryConta

--- src/util/TipoRepo.java
+++ src/util/TipoRepo.java
@@ -2,7 +2,20 @@ package util;
 
 public enum TipoRepo {
 	
-		ARRAY,SERIAL,JDBC,TREESET,HASHMAP;
+		ARRAY(0),SERIAL(1),JDBC(2),TREESET(3),HASHMAP(4);
 			
+	private final int tipo;
+
+	private TipoRepo(int tipo) {
+		this.tipo = tipo;
+	}
 	
+	public static TipoRepo forInt(int valor) {
+		for(TipoRepo tipoRepo : values()) {
+			if (tipoRepo.tipo == valor)
+				return tipoRepo;
+		}
+		
+		return null;
+	}
 }
